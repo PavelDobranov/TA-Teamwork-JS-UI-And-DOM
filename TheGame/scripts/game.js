4c7fcc28c695fc18ae2f4378820afd18a788503f
PavelDobranov <pdobranov@gmail.com>
2015-07-25 16:08:23 +0300

    Added player animations

--- TheGame/scripts/game.js
+++ TheGame/scripts/game.js
@@ -2,7 +2,7 @@ window.addEventListener('load', function() {
   var Q = window.Q = Quintus({
       development: true
     })
-    .include('Scenes, Sprites, 2D, Input, Touch, UI, TMX, Audio')
+    .include('Scenes, Sprites, 2D, Input, Touch, UI, TMX, Anim')
     .include('player')
     .setup({
       width: 320,
@@ -27,6 +27,13 @@ window.addEventListener('load', function() {
   //load assets
   Q.loadTMX('testMapWithPlayer.tmx, player.json', function() {
     Q.compileSheets('player-spritesheet.png', 'player.json');
+    Q.animations("player", {
+      walk_right: { frames: [0,1,2], rate: 1/2, flip: false, loop: true },
+      walk_left: { frames:  [3,4,5], rate: 1/4, flip:"x", loop: true }
+    });
+
+
+
     Q.stageScene('testMap');
   });
 });
--- TheGame/scripts/player.js
+++ TheGame/scripts/player.js
@@ -2,12 +2,17 @@ Quintus.player = function (Q) {
   Q.Sprite.extend('Player', {
     init: function(p) {
       this._super(p, {
-        sheet: 'player',
+        sheet: 'player-spritesheet',
+        sprite: 'player',
         jumpSpeed: -480,
-        speed: 100
+        speed: 200
       });
 
-      this.add('2d, platformerControls');
+
+      this.add('2d, platformerControls, animation');
+    },
+    step: function (dt) {
+      this.play("walk_right");
     }
   });
 };
