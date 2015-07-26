9fe8e8d36ddaa97d7c9d2c84f703d81aac55c04e
PetarMetodiev <petar_mih_met@abv.bg>
2015-07-26 17:47:35 +0300

    Enemies can now walk on platforms without falling

--- SourceCode/public/js/game/enemy.js
+++ SourceCode/public/js/game/enemy.js
@@ -12,6 +12,30 @@ Quintus.enemy = function (Q) {
 		},
 		step: function (dt) {
 			this.play('walk');
+			var dirX = this.p.vx / Math.abs(this.p.vx);
+			var ground = Q.stage().locate(this.p.x, this.p.y + this.p.h / 2 + 1, Q.SPRITE_DEFAULT); // Q.SPRITE_DEFAULT??
+			var nextElement = Q.stage().locate(this.p.x + dirX * this.p.w / 2 + dirX, this.p.y + this.p.h / 2 + 1, Q.SPRITE_DEFAULT); // Q.SPRITE_DEFAULT??
+			var nextTile;
+
+			if (nextElement instanceof Q.TileLayer) {
+				nextTile = true;
+			}
+
+			if (!nextTile && ground) {
+				if (this.p.vx>0) {
+					// if (this.p.defaultDirection === 'right') {
+					// 	this.p.flip = 'x'
+					// } else {
+					// 	this.p.flip = false;
+					// }
+					this.p.flip = 'x';
+				} else {
+					if (this.p.defaultDirection = 'left') {
+						this.p.flip = false;
+					}
+				}
+				this.p.vx = -this.p.vx;
+			}
 		}
 	})
 };
