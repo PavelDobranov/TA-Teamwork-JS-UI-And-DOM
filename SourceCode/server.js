e408c5e16fc87ff5166ddbecb8b1a0fa30353b1c
PavelDobranov <pdobranov@gmail.com>
2015-07-26 12:37:36 +0300

    Added server.js

--- SourceCode/server.js
+++ SourceCode/server.js
@@ -1,1 +1,13 @@
+var express = require('express');
 
+var server = express(),
+  env = process.env.NODE_ENV || 'development',
+  config = require('./server/config/config')[env];
+
+require('./server/config/express')(server, config);
+require('./server/config/routes')(server);
+
+server.listen(config.port, function () {
+  console.info('Server running on port: ' + config.port);
+  console.info('Environment: ' + env);
+});
