f6c2ea9ed9317c5de7ac3a96270e3f1c1e330f77
zhenyaracheva <zhenia.racheva@gmail.com>
2015-07-28 09:29:28 +0300

    Player dies when fall in hole

--- SourceCode/public/js/game/player.js
+++ SourceCode/public/js/game/player.js
@@ -1,54 +1,62 @@
 Quintus.player = function (Q) {
-  'use strict';
+    'use strict';
 
-  Q.Sprite.extend('Player', {
-    init: function (p) {
-      this._super(p, {
-        sheet: 'player-spritesheet',
-        sprite: 'player',
-        type: Q.SPRITE_PLAYER,
-        collisionMask: Q.SPRITE_ENEMY,
-        jumpSpeed: Q.PLAYER_JUMP_SPEED,
-        speed: Q.PLAYER_SPEED,
-      });
+    Q.Sprite.extend('Player', {
+        init: function (p) {
+            this._super(p, {
+                sheet: 'player-spritesheet',
+                sprite: 'player',
+                type: Q.SPRITE_PLAYER,
+                collisionMask: Q.SPRITE_ENEMY,
+                jumpSpeed: Q.PLAYER_JUMP_SPEED,
+                speed: Q.PLAYER_SPEED
+            });
 
-      this.add('2d, platformerControls, animation');
-      this.add('weapon');
+            this.add('2d, platformerControls, animation');
+            this.add('weapon');
 
-      Q.input.on('fire', this, 'shoot');
-      this.on('fired', this, 'launchBullet');
-      this.on('hit.sprite', 'die');
-    },
-    shoot: function () {
-      var that = this;
+            Q.input.on('fire', this, 'shoot');
+            this.on('fired', this, 'launchBullet');
+            this.on('hit.sprite', 'die');
+        },
 
-      if (!that.p.canShoot) {
-        return;
-      }
+        shoot: function () {
+            var that = this;
 
-      this.play(this.p.direction + 'Shoot', 1);
-    },
-    step: function (dt) {
-      if (this.p.vx !== 0 && this.p.vy === 0) {
-        this.play(this.p.direction + 'Run');
-      } else if (this.p.vy < 0) {
-        this.play(this.p.direction + 'Jump');
-      } else if (this.p.vy > 0) {
-        this.play(this.p.direction + 'Fall');
-      } else {
-        this.play(this.p.direction + 'Idle');
-      }
+            if (!that.p.canShoot) {
+                return;
+            }
 
-      if (Q.inputs['down']) {
-        this.play(this.p.direction + 'Duck');
-      }
-    },
-    die: function (col) {
-      if (col.obj.isA('Enemy')) {
-        console.log(col);
-        Q.stageScene('endGame', 1, this.p);
-        this.destroy();
-      }
-    }
-  });
+            this.play(this.p.direction + 'Shoot', 1);
+        },
+
+        step: function (dt) {
+            if (this.p.vx !== 0 && this.p.vy === 0) {
+                this.play(this.p.direction + 'Run');
+            } else if (this.p.vy < 0) {
+                this.play(this.p.direction + 'Jump');
+            } else if (this.p.vy > 0) {
+                this.play(this.p.direction + 'Fall');
+            } else {
+                this.play(this.p.direction + 'Idle');
+            }
+
+            if (this.p.y > Q.height) {
+                Q.stageScene('endGame', 1, this.p);
+                this.destroy();
+            }
+
+            if (Q.inputs['down']) {
+                this.play(this.p.direction + 'Duck');
+            }
+        },
+
+        die: function (col) {
+            if (col.obj.isA('Enemy')) {
+                console.log(col);
+                Q.stageScene('endGame', 1, this.p);
+                this.destroy();
+            }
+        }
+    });
 };
