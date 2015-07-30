b7fb3aea2df4b9a068404a6dc2ca891edf428e43
PavelDobranov <pdobranov@gmail.com>
2015-07-30 17:08:13 +0300

    Implemented server side partial views response

--- SourceCode/server/config/express.js
+++ SourceCode/server/config/express.js
@@ -1,8 +1,11 @@
 var express = require('express'),
+  bodyParser = require('body-parser'),
   path = require('path');
 
 module.exports = function(server, config) {
   server.set('view engine', 'jade');
   server.set('views', path.join(config.rootPath, 'server/views'));
+  server.use(bodyParser.urlencoded({ extended: false }));
+  server.use(bodyParser.json())
   server.use(express.static(path.join(config.rootPath, 'public')));
 };
--- SourceCode/server/config/routes.js
+++ SourceCode/server/config/routes.js
@@ -1,4 +1,16 @@
 module.exports = function(server) {
+  server.get('/play', function(req, res) {
+    res.render('partials/play');
+  });
+
+  server.get('/requirements', function(req, res) {
+    res.render('partials/requirements');
+  });
+
+  server.get('/documentation', function(req, res) {
+    res.render('partials/documentation');
+  });
+
   server.get('*', function(req, res) {
     res.render('index');
   });
